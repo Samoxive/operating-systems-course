6bf45d87341f942eb4d5173e630f2c3ad65ee736
Şamil NART <samil.nart@gmail.com>
2018-10-03 17:58:17 +0300

    Seperate test code from main

--- Homeworks/Homework_1/main.c
+++ Homeworks/Homework_1/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include "file_system.h"
 
-int main() {
+void test() {
     file_system* fs = file_system_create_file_system();
     file_system_touch_folder(fs, "hello");
     file_system_touch_file(fs, "world");
     file_system_cd(fs, "hello");
     file_system_touch_file(fs, "foo");
+    file_system_touch_file(fs, "foo1");
+    file_system_touch_file(fs, "foo2");
+    file_system_touch_file(fs, "foo3");
+    file_system_touch_file(fs, "foo4");
     file_system_cdup(fs);
     file_system_lsrecursive(fs);
+    file_system_cleanup_file_system(fs);
+}
+
+int main() {
+    test();
+    return 0;
 }
