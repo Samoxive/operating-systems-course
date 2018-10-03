15b563e04124f80538e2e015024d51a7714a74c7
Şamil NART <samil.nart@gmail.com>
2018-10-03 18:31:18 +0300

    Malloc file/folder names so that we don't free string literals

--- Homeworks/Homework_1/main.c
+++ Homeworks/Homework_1/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "file_system.h"
 
 void test() {
     file_system* fs = file_system_create_file_system();
-    file_system_touch_folder(fs, "hello");
-    file_system_touch_file(fs, "world");
+    file_system_touch_folder(fs, strdup("hello"));
+    file_system_touch_file(fs, strdup("world"));
     file_system_cd(fs, "hello");
-    file_system_touch_file(fs, "foo");
-    file_system_touch_file(fs, "foo1");
-    file_system_touch_file(fs, "foo2");
-    file_system_touch_file(fs, "foo3");
-    file_system_touch_file(fs, "foo4");
+    file_system_touch_file(fs, strdup("foo"));
+    file_system_touch_file(fs, strdup("foo1"));
+    file_system_touch_file(fs, strdup("foo2"));
+    file_system_touch_file(fs, strdup("foo3"));
+    file_system_touch_file(fs, strdup("foo4"));
     file_system_cdup(fs);
     file_system_lsrecursive(fs);
     file_system_cleanup_file_system(fs);
