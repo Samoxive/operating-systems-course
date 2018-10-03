13e75d54124cc7549b22a044e25af28dd26e9a9b
Şamil NART <samil.nart@gmail.com>
2018-10-03 11:11:08 +0300

    Add function signatures to the header

--- Homeworks/Homework_1/file_system.h
+++ Homeworks/Homework_1/file_system.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdbool.h>
 #include <time.h>
 #include "typedefs.h"
 
@@ -33,3 +34,41 @@ typedef struct file_system {
     struct file_system_folder* cursor;
 
 } file_system;
+
+void file_system_panic(char* message, file_system* fs);
+file_system_entity file_system_create_entity(char* name,
+                                             file_system_entity_type type);
+file_system_folder* file_system_create_folder(char* name);
+file_system_file* file_system_create_file(char* name);
+file_system* file_system_create_file_system();
+void file_system_cleanup_file(file_system_file* file);
+void file_system_cleanup_folder(file_system* fs, file_system_folder* folder);
+void file_system_cleanup_entity(file_system* fs, file_system_entity* entity);
+file_system_entity* file_system_get_entity_by_name(file_system* fs, char* name);
+bool file_system_name_exists_in_folder(file_system* fs, char* name);
+void file_system_add_entity_to_folder(file_system* fs,
+                                      file_system_entity* entity);
+file_system_entity* file_system_remove_entity_from_folder_fiber(
+    file_system* fs,
+    file_system_entity* fiber_head,
+    char* entity_name,
+    bool* removed);
+void file_system_remove_entity_from_folder(file_system* fs, char* entity_name);
+void file_system_touch_file(file_system* fs, char* file_name);
+void file_system_touch_folder(file_system* fs, char* folder_name);
+void file_system_print_file(file_system_file* file);
+void file_system_print_folder(file_system_folder* folder);
+void file_system_print_entity(file_system* fs, file_system_entity* entity);
+void file_system_ls(file_system* fs);
+void file_system_lsrecursive_impl(file_system* fs,
+                                  file_system_folder* folder,
+                                  int level);
+void file_system_lsrecursive(file_system* fs);
+void file_system_pwd(file_system* fs);
+void file_system_cd(file_system* fs, char* target_name);
+void file_system_cdup(file_system* fs);
+void file_system_modify_file(file_system* fs, char* name, char* new_content);
+void file_system_rm(file_system* fs, char* name);
+void file_system_mov(file_system* fs,
+                     char* target_name,
+                     char* target_folder_name);
