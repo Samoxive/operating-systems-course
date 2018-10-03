339e3a73235771e381c18c7933dd376c4bd7d417
Şamil NART <samil.nart@gmail.com>
2018-10-03 22:52:45 +0300

    Add more tests

--- Homeworks/Homework_1/main.c
+++ Homeworks/Homework_1/main.c
@@ -12,6 +12,16 @@ void test() {
     file_system_touch_file(fs, strdup("foo2"));
     file_system_touch_file(fs, strdup("foo3"));
     file_system_touch_file(fs, strdup("foo4"));
+    file_system_ls(fs);
+    file_system_pwd(fs);
+    file_system_modify_file(fs, "foo", strdup("hello i am smart"));
+    file_system_rm(fs, "foo3");
+    file_system_cdup(fs);
+    file_system_mov(fs, "world", "hello");
+    file_system_lsrecursive(fs);
+    file_system_cd(fs, "hello");
+    file_system_touch_folder(fs, strdup("nested_folder"));
+    file_system_mov(fs, "world", "nested_folder");
     file_system_cdup(fs);
     file_system_lsrecursive(fs);
     file_system_cleanup_file_system(fs);
