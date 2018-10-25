4bbee5ef03d06c5206a7893e7b8254b3346dc1c5
Şamil NART <samil.nart@gmail.com>
2018-10-25 13:07:59 +0300

    Implement color parsing from string

--- Projects/Project_1/project_util.c
+++ Projects/Project_1/project_util.c
@@ -1,4 +1,71 @@
-#include "project_util.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-COLOR* split_input_into_colors(char* string) {}
+#include "c_string.h"
+#include "project_util.h"
+
+COLOR get_color_from_string(char* str) {
+    if (c_string_equals(str, "red")) {
+        return RED;
+    } else if (c_string_equals(str, "green")) {
+        return GREEN;
+    } else if (c_string_equals(str, "blue")) {
+        return BLUE;
+    } else if (c_string_equals(str, "orange")) {
+        return ORANGE;
+    } else if (c_string_equals(str, "yellow")) {
+        return YELLOW;
+    } else if (c_string_equals(str, "white")) {
+        return WHITE;
+    } else if (c_string_equals(str, "black")) {
+        return BLACK;
+    } else if (c_string_equals(str, "purple")) {
+        return PURPLE;
+    } else {
+        return INVALID_COLOR;
+    }
+}
+
+char* get_string_from_color(COLOR color) {
+    switch (color) {
+        case RED:
+            return "red";
+        case GREEN:
+            return "green";
+        case BLUE:
+            return "blue";
+        case ORANGE:
+            return "orange";
+        case YELLOW:
+            return "yellow";
+        case WHITE:
+            return "white";
+        case BLACK:
+            return "black";
+        case PURPLE:
+            return "purple";
+        case INVALID_COLOR:
+            return "invalid_color";
+    }
+}
+
+color_list split_input_into_colors(char* string) {
+    int i = 0;
+    int capacity = 8;
+    COLOR* colors = malloc(capacity * sizeof(COLOR));
+    char buf[128] = {0};
+    while (sscanf(string, " %[^,\n],", buf) != EOF) {
+        COLOR color = get_color_from_string(buf);
+
+        if (i == capacity) {
+            capacity *= 2;
+            colors = realloc(colors, capacity);
+        }
+
+        colors[i] = color;
+    }
+
+    color_list list = {.colors = colors, .size = i};
+    return list;
+}
--- Projects/Project_1/project_util.h
+++ Projects/Project_1/project_util.h
@@ -1,3 +1,22 @@
 #pragma once
 
-typedef enum { RED, GREEN, BLUE, ORANGE, YELLOW, WHITE, BLACK, PURPLE } COLOR;
+typedef enum {
+    RED,
+    GREEN,
+    BLUE,
+    ORANGE,
+    YELLOW,
+    WHITE,
+    BLACK,
+    PURPLE,
+    INVALID_COLOR,
+} COLOR;
+
+typedef struct {
+    COLOR* colors;
+    int size;
+} color_list;
+
+COLOR get_color_from_string(char*);
+char* get_string_from_color(COLOR);
+color_list split_input_into_colors(char*);
