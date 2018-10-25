4005eb6a09636cfc06166942b28fb4091dbd491c
Şamil NART <samil.nart@gmail.com>
2018-10-25 13:07:13 +0300

    Make common files library and seperate executables from them

---
+++ Projects/Project_1/common_include.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "c_string.h"
+#include "file.h"
+#include "project_util.h"
+#include "typedefs.h"
--- Projects/Project_1/main.c
+++ Projects/Project_1/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include "file.h"
+#include "common_include.h"
 
 int main(int argc, char** argv) {
-    char* content = read_file_to_string("../main.c");
+    char* content = read_file_to_string("../input.txt");
     printf("%s\n", content);
+    color_list list = split_input_into_colors(content);
+    for (int i = 0; i < list.size; i++) {
+        printf("%s\n", get_string_from_color(list.colors[i]));
+    }
+
     return 0;
 }
