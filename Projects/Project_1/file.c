0fb1bdef89e19b7c347103968f71d90f5201a628
Şamil NART <samil.nart@gmail.com>
2018-10-26 11:23:04 +0300

    Add write and read struct to and from file functions

--- Projects/Project_1/file.c
+++ Projects/Project_1/file.c
@@ -9,7 +9,7 @@ char* read_file_to_string(char* file_name) {
         return null;
     }
 
-    int fd;
+    i32 fd;
     if ((fd = fileno(file)) == -1) {
         fclose(file);
         return null;
@@ -21,7 +21,7 @@ char* read_file_to_string(char* file_name) {
     }
     i64 size = file_stats.st_size;
     char* buf = malloc(size + 1);
-    buf[size] = '\0'; // fread doesn't always null terminate as it claims to
+    buf[size] = '\0';  // fread doesn't always null terminate as it claims to
     if (!fread(buf, sizeof(char), size, file)) {
         fclose(file);
         return null;
@@ -29,4 +29,26 @@ char* read_file_to_string(char* file_name) {
 
     fclose(file);
     return buf;
+}
+
+bool write_struct_to_file(char* file_name, void* data, i32 size) {
+    FILE* file = fopen(file_name, "wb");
+    if (file == null) {
+        return true;
+    }
+
+    fwrite(data, size, 1, file);
+    fclose(file);
+    return false;
+}
+
+bool read_struct_from_file(char* file_name, void* data, i32 size) {
+    FILE* file = fopen(file_name, "rb");
+    if (file == null) {
+        return true;
+    }
+
+    fread(data, size, 1, file);
+    fclose(file);
+    return false;
 }
--- Projects/Project_1/file.h
+++ Projects/Project_1/file.h
@@ -6,3 +6,5 @@
 #include "c_string.h"
 
 char* read_file_to_string(char*);
+bool write_struct_to_file(char* file_name, void* data, i32 size);
+bool read_struct_from_file(char* file_name, void* data, i32 size);
