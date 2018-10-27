4ed12ecf1a92e70603e54262c5a08a9a92b9f386
Şamil NART <samil.nart@gmail.com>
2018-10-27 15:13:36 +0300

    Get rid of unused variables

--- Projects/Project_1/main.c
+++ Projects/Project_1/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "common_include.h"
 
-i32 main(i32 argc, char** argv) {
+i32 main(void) {
     char* content = read_file_to_string("../input.txt");
     printf("%s\n", content);
-    color_parse_result result = parse_string_into_colors(content);
+    parse_string_into_colors(content);
     free(content);
     return 0;
 }
