abec00086b91aa3ea1704066f4267f98a8b28185
Şamil NART <samil.nart@gmail.com>
2018-10-28 12:43:32 +0300

    Use single shared memory to rule them all

--- Projects/Project_1/psearch2a.c
+++ Projects/Project_1/psearch2a.c
@@ -10,6 +10,7 @@
 #include "common_include.h"
 
 const char* psearch2aslave_exe = "./build/psearch2aslave";
+const char* psearch2a_shm_name = "/psearch";
 
 i32 main(i32 argc, char** argv) {
     if (argc < 5) {
@@ -27,50 +28,39 @@ i32 main(i32 argc, char** argv) {
     i32 input_files_count = argc - 4;
     char* output_file_name = argv[argc - 1];
     char** input_files_names = c_string_array_subarray(argv, 3, argc - 1);
-    i32* fd_results = malloc(input_files_count * sizeof(i32));
-    color_parse_result** parse_results =
-        malloc(input_files_count * sizeof(color_parse_result*));
-    char** results_shm_names = malloc(input_files_count * sizeof(char*));
-    for (i32 i = 0; i < input_files_count; i++) {
-        char* shm_name = c_string_concat("/psearch2a_", input_files_names[i]);
-        for (usize i = 1; i < strlen(shm_name); i++) {
-            if (shm_name[i] == '/') {
-                shm_name[i] = '.';
-            }
-        }
+    i32 shm_fd =
+        shm_open(psearch2a_shm_name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (shm_fd == -1) {
+        printf("Could not open shared memory blocks.\n");
+        exit(-1);
+    }
+    if (ftruncate(shm_fd, input_files_count * sizeof(color_parse_result)) ==
+        -1) {
+        printf("Could not extend shared memory blocks.\n");
+        exit(-1);
+    }
 
-        i32 shm_fd = shm_open(shm_name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-        if (shm_fd == -1) {
-            printf("Could not open shared memory blocks. main.\n");
-            exit(-1);
-        }
-        if (ftruncate(shm_fd, sizeof(color_parse_result)) == -1) {
-            printf("Could not extend shared memory blocks.\n");
-            exit(-1);
-        }
-
-        fd_results[i] = shm_fd;
-        results_shm_names[i] = shm_name;
-
-        color_parse_result* result =
-            mmap(null, sizeof(color_parse_result), PROT_READ | PROT_WRITE,
-                 MAP_SHARED, fd_results[i], 0);
-        if (result == MAP_FAILED) {
-            printf("Could not mmap shared memory blocks.\n");
-            exit(-1);
-        }
-        parse_results[i] = result;
+    color_parse_result* results =
+        mmap(null, input_files_count * sizeof(color_parse_result),
+             PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (results == MAP_FAILED) {
+        printf("Could not mmap shared memory blocks.\n");
+        exit(-1);
     }
 
     for (i32 i = 0; i < input_files_count; i++) {
         i32 f = fork();
         if (f == 0) {
+            char index_str[12];
+            char length_str[12];
+            sprintf(index_str, "%d", i);
+            sprintf(length_str, "%d", input_files_count);
             // exec* family functions don't give a damn about acting reasonable,
             // given argv array will be passed to the slave directly, so to
             // emulate normal behavior, we give the slave executable's path to
             // argv as well. An hour was wasted on this.
             execl(psearch2aslave_exe, psearch2aslave_exe, target_color_string,
-                  input_files_names[i], null);
+                  input_files_names[i], index_str, length_str, null);
 
             printf("Slave process failed. Errno: %d\n", errno);
             exit(-1);
@@ -81,20 +71,9 @@ i32 main(i32 argc, char** argv) {
     while (wait(&wstatus) > 0)
         ;
 
-    color_parse_result* results =
-        malloc(input_files_count * sizeof(color_parse_result));
-    for (i32 i = 0; i < input_files_count; i++) {
-        results[i] = *(parse_results[i]);
-        munmap(parse_results[i], sizeof(color_parse_result));
-        shm_unlink(results_shm_names[i]);
-        free(results_shm_names[i]);
-    }
-    free(fd_results);
-    free(results_shm_names);
-    free(parse_results);
-
     write_final_output_to_file(target_color, input_files_count, results,
                                input_files_names, output_file_name);
+    munmap(results, input_files_count * sizeof(color_parse_result));
+    shm_unlink(psearch2a_shm_name);
     free(input_files_names);
-    free(results);
 }
--- Projects/Project_1/psearch2aslave.c
+++ Projects/Project_1/psearch2aslave.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <semaphore.h>
 #include <stdio.h>
@@ -8,13 +9,22 @@
 #include <unistd.h>
 #include "common_include.h"
 
+const char* psearch2a_shm_name = "/psearch";
+
 i32 main(i32 argc, char** argv) {
-    if (argc != 3) {
+    if (argc != 5) {
         printf("Invalid command line input.\n");
         exit(-1);
     }
 
     char* input_file_name = argv[2];
+    i32 result_index = atoi(argv[3]);
+    i32 result_length = atoi(argv[4]);
+    printf("%d %d\n", result_index, result_length);
+    if (result_index < 0 || result_length < 0) {
+        printf("Invalid command line input.\n");
+        exit(-1);
+    }
     char* file_content = read_file_to_string(input_file_name);
     if (file_content == null) {
         printf("Could not read given file.\n");
@@ -22,32 +32,25 @@ i32 main(i32 argc, char** argv) {
     }
 
     color_parse_result result = parse_string_into_colors(file_content);
-    char* output_shm_name = c_string_concat("/psearch2a_", input_file_name);
-    for (usize i = 1; i < strlen(output_shm_name); i++) {
-        if (output_shm_name[i] == '/') {
-            output_shm_name[i] = '.';
-        }
-    }
 
-    i32 shm_fd = shm_open(output_shm_name, O_RDWR, S_IRUSR | S_IWUSR);
+    i32 shm_fd = shm_open(psearch2a_shm_name, O_RDWR, S_IRUSR | S_IWUSR);
     if (shm_fd == -1) {
+        char s[100];
         printf("Could not open shared memory blocks.\n");
         exit(-1);
     }
 
     color_parse_result* shm_result =
-        mmap(null, sizeof(color_parse_result), PROT_READ | PROT_WRITE,
-             MAP_SHARED, shm_fd, 0);
+        mmap(null, result_length * sizeof(color_parse_result),
+             PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (shm_result == MAP_FAILED) {
         printf("Could not mmap shared memory blocks.\n");
         exit(-1);
     }
 
-    *shm_result = result;
+    shm_result[result_index] = result;
 
-    munmap(shm_result, sizeof(color_parse_result));
-    shm_unlink(output_shm_name);
+    munmap(shm_result, result_length * sizeof(color_parse_result));
     free(file_content);
-    free(output_shm_name);
     exit(0);
 }
